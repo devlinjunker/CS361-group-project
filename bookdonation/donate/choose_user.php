a4c76a5215c174198887b12bbd485d8dec5179f9
Jordan McConnell <mcconnjo@onid.orst.edu>
2012-03-09 15:02:26 -0800

    tried to fix bug again

--- bookdonation/donate/choose_user.php
+++ bookdonation/donate/choose_user.php
@@ -1,22 +1,24 @@
-<form id="choose_user_form" name="choose_user_form" method="post" action="">
 
 <?php
 	$query = "SELECT name, message, accountid, requestid FROM bookrequests NATURAL JOIN accounts WHERE isbn = '$isbn' AND sent = 0 AND received = 0 AND returned = 0 ORDER BY date ASC";
 
 	$result = mysql_query($query);
 	if(!$result){
+		echo "didnt work";
 		die('failed to retrieve users');
 	}
 	if(mysql_num_rows($result) == 0){
 	
 	}	
-	else{	$i = 0;
+	else{	
+		$i = 0;
+		echo "<form id=\"choose_user_form\" name=\"choose_user_form\" method=\"post\" action=\"\">";
 		while($row = mysql_fetch_assoc($result)){
-			echo "<input type=\"checkbox\"i ";
+			echo "<input type=\"radio\" ";
 			if($i == 0){
 				echo "selected=\"selected\"";
 			}	
-			echo " name=\"requestid\" value=\"$row['requestid']\"/> $row['name']  Reason: $row['message']</br>";
+			echo " name=\"requestid\" value=\"$row['requestid']\" /> $row['name']  Reason: $row['message']</br>";
 			$i++;
 		}
 
