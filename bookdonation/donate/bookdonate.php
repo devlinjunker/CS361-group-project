3fd0d61d8f507664f1902c96611893613663f0b7
Jordan McConnell <mcconnjo@onid.orst.edu>
2012-03-09 14:38:58 -0800

    updated bookdonate.php and included the choose_user.php file

--- bookdonation/donate/bookdonate.php
+++ bookdonation/donate/bookdonate.php
@@ -14,19 +14,27 @@
 	$isbn = mysql_real_escape_string($_POST['book_isbn']);
 	$name = mysql_real_escape_string($_POST['book_name']);
 	$condition = mysql_real_escape_string($_POST['book_condition2']);
+	switch($_POST['RadioGroup1']){
+		case "book_nextuser":
+			$sql = "INSERT INTO  `junkerd-db`.`bookdonations` (`donationid` ,`isbn` ,`accountid` ,`condition` ,`available`)VALUES (NULL ,  '$isbn',  '$USERID',  '$condition',  '0')";
 	
-	$sql = "INSERT INTO  `junkerd-db`.`bookdonations` (`donationid` ,`isbn` ,`accountid` ,`condition` ,`available`)VALUES (NULL ,  '$isbn',  '$USERID',  '$condition',  '0')";
+			#echo $sql;	
+			$result = mysql_query($sql, $mysql_handle);
 	
-	#echo $sql;	
-	$result = mysql_query($sql, $mysql_handle);
-	
-	if($result == 1){
-		echo "<div id='message'> Succesfully Donated Book... <a href='../account'> Return to Account Here</a></div>";
+			if($result == 1){
+				echo "<div id='message'> Succesfully Donated Book... <a href='../account'> Return to Account Here</a></div>";
 		
-		header('refresh: 5; url=../account');
-	}else{
-		
-	}
+				header('refresh: 5; url=../account');
+				break;
+			}else{
+				break;	
+			}
+		case "book_listuser":
+			include("choose_user.php");
+			break;
+		default:
+			break;
+	}			
 	
 ?>
 
