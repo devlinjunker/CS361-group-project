8e93f1ec65ad8b9ed211819321ffb0567266a3a9
Francis Vo <firefreakfrancis@gmail.com>
2012-03-09 14:20:43 -0800

    Donate page working

---
+++ bookdonation/donate/bookdonate.php
@@ -0,0 +1,34 @@
+<?php  
+	include '../header.html';
+	include "../log.php";
+	
+	
+	echo "<style> #message{
+	position:relative;
+	left:200px;
+	top:50px;
+	}</style>";
+	
+	$sitepage = "Donate";
+	
+	$isbn = mysql_real_escape_string($_POST['book_isbn']);
+	$name = mysql_real_escape_string($_POST['book_name']);
+	$condition = mysql_real_escape_string($_POST['book_condition2']);
+	
+	$sql = "INSERT INTO  `junkerd-db`.`bookdonations` (`donationid` ,`isbn` ,`accountid` ,`condition` ,`available`)VALUES (NULL ,  '$isbn',  '$USERID',  '$condition',  '0')";
+	
+	#echo $sql;	
+	$result = mysql_query($sql, $mysql_handle);
+	
+	if($result == 1){
+		echo "<div id='message'> Succesfully Donated Book... <a href='../account'> Return to Account Here</a></div>";
+		
+		header('refresh: 5; url=../account');
+	}else{
+		
+	}
+	
+?>
+
+
+<?php include '../footer.html' ?>
