66c691ce928ca238c88fb3a81d33cc59e80ee6e4
Jordan McConnell <mcconnjo@onid.orst.edu>
2012-03-09 15:12:08 -0800

    trying to fix this bug

--- bookdonation/donate/choose_user.php
+++ bookdonation/donate/choose_user.php
@@ -1,15 +1,15 @@
 
 <?php
-	$query = "SELECT name, message, accountid, requestid FROM bookrequests NATURAL JOIN accounts WHERE isbn = '$isbn' AND sent = 0 AND received = 0 AND returned = 0 ORDER BY date ASC";
+	$query = "SELECT name, message, accountid, requestid FROM bookrequests NATURAL JOIN accounts WHERE isbn = '$isbn' accountid <> '$USERID' AND sent = 0 AND received = 0 AND returned = 0 ORDER BY date ASC";
 
-	$result = mysql_query($query);
+	$result = mysql_query($query,$mysql_handle);
 	if(!$result){
 		echo "didnt work";
 		die('failed to retrieve users');
 	}
 	if(mysql_num_rows($result) == 0){
 	
-	}	
+/*	}	
 	else{	
 		$i = 0;
 		echo "<form id=\"choose_user_form\" name=\"choose_user_form\" method=\"post\" action=\"\">";
@@ -25,5 +25,5 @@
 		echo "<input type=\"submit\" value=\"submit\" />";
 		echo "</form>";
 		break;
-	}
+	}*/
 ?>
