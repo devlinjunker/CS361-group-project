969079bfc104e857153aa667863d8c13c6b9263a
Kyle Mannari <asiancarz14@msn.com>
2012-03-09 15:32:21 -0800

    updated newbook

--- bookdonation/request/newbook.php
+++ bookdonation/request/newbook.php
@@ -8,7 +8,7 @@
 	$author = mysql_real_escape_string($_POST['author']);
 	
 	$query = "INSERT INTO books (`isbn`, `name`, `author`)
-			  VALUE ('$isbn', '$title', '$author')"
+			  VALUE ('$isbn', '$title', '$author')";
 			  
 	mysql_query($query, $mysql_handle);
 
