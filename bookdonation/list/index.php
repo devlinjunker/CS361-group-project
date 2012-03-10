c3c2a6ede11635ce0ad6c57148cfffb649183752
Kyle Mannari <asiancarz14@msn.com>
2012-03-09 17:24:40 -0800

    added book list

--- bookdonation/list/index.php
+++ bookdonation/list/index.php
@@ -5,6 +5,41 @@
 
 <h2><?php echo $sitepage; ?></h2>
 
+	<h3>Availability</h3>
+	<table id="booklist">
+		<tr>
+			<th>ISBN</th>
+			<th>Title</th>
+			<th>Condition</th>
+			<th>Available</th>
+		</tr>
 
+<?php
+	$query_donation = "SELECT  `available` ,  `condition` ,  `bookdonations`.`isbn`, `name`
+						FROM  bookdonations, books
+						WHERE bookdonations.isbn = books.isbn" ;
+
+	$result_donation = mysql_query($query_donation, $mysql_handle);
+		
+	while($result_don = mysql_fetch_array($result_donation)) { 
+?>
+		
+		<tr>
+			<td><?php echo $result_don[2]; ?></td>
+			<td><?php echo $result_don[3]; ?></td>
+			<td><?php echo $result_don[1]; ?></td>
+			<td><?php 
+					switch($result_don[0]) {
+						case 1:
+							echo 'yes';
+							break;
+						default:
+							echo 'no';
+					}
+				
+				?></td>
+		</tr>
+
+<?php	} ?>
 
 <?php include '../footer.html' ?>
